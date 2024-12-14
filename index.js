3e26c762bf8022b5841fc968d1adfaf77e5bc8d0
Md Aminul Islam <mr.aminul118@gmail.com>
2024-12-14 15:31:01 +0600

    Basic server and job post and read

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.b4uwa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const jobCollections = client.db("jobsDB").collection("jobs");
+    app.post("/job", async (req, res) => {
+      const newJob = req.body;
+      const result = await jobCollections.insertOne(newJob);
+      res.send(result);
+    });
+    app.get("/jobs", async (req, res) => {
+      const jobs = await jobCollections.find().toArray();
+      res.send(jobs);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello from SoloSphere Server....");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
