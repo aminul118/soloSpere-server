0a6979e72d79e726cc04bb2f92612bd6f4df9eb4
Md Aminul Islam <mr.aminul118@gmail.com>
2024-12-19 23:22:06 +0600

    Job get by id, bids post and increase bids count

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -25,14 +25,59 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const jobCollections = client.db("jobsDB").collection("jobs");
+    const bidCollections = client.db("jobsDB").collection("bids");
     app.post("/job", async (req, res) => {
       const newJob = req.body;
       const result = await jobCollections.insertOne(newJob);
       res.send(result);
     });
+
     app.get("/jobs", async (req, res) => {
       const jobs = await jobCollections.find().toArray();
       res.send(jobs);
+    });
+    
+    app.get("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCollections.findOne(query);
+      res.send(result);
+    });
+
+    // Bid
+    app.post("/bids", async (req, res) => {
+      const newBid = req.body;
+      // one user can bit once in a job
+      const query = {
+        applicant_email: newBid.applicant_email,
+        job_id: newBid.job_id,
+      };
+      const alreadyExits = await bidCollections.findOne(query);
+      if (alreadyExits)
+        return res.status(400).send("You already bided this job");
+
+      // Save data in bids collections
+      const result = await bidCollections.insertOne(newBid);
+      // Increment bid count when place a bid
+      const filter = { _id: new ObjectId(newBid.job_id) };
+      const update = {
+        $inc: {
+          bids_count: 1,
+        },
+      };
+      const updateBidCount = await jobCollections.updateOne(filter, update);
+
+      res.send(result);
+    });
+
+    // Bids found individually by user email
+    app.get("/my-bids/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = {
+        applicant_email: email,
+      };
+      const result = await bidCollections.find(query).toArray();
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
